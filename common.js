656a9734807b5711587d7942619aaacabcbde2fa
BuildTools <unconfigured@null.spigotmc.org>
2021-12-09 09:11:00 +0800

    d

--- common.js
+++ common.js
@@ -60,6 +60,7 @@ function send_data() {
         let send_buffer = Buffer.concat([num_buffer, data]);
 
         let {id, count} = get_noblock_tunnel(clients, tunnel_num);
+        console.log(id, count);
         if(id == -1) {
             clients[count].write(send_buffer);
             return false;
