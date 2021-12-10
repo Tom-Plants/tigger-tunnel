3762c8594832c597b4af3646b61b658906d6dbe4
BuildTools <unconfigured@null.spigotmc.org>
2021-12-10 23:14:47 +0800

    dd

--- clients_controller.js
+++ clients_controller.js
@@ -46,10 +46,7 @@ function get_noblock_client() {
     let num = client_pointer;
     while(true) {
         if(clients[num] != undefined && clients[num]._paused == false && clients[num]._state == 1) {
-            client_pointer = num + 1;
-            if(client_pointer == tunnel_num) {
-                client_pointer = 0;
-            }
+            client_pointer = num;
             return clients[num];
         }
         num ++;
