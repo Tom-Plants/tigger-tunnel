06368db29074791add1a3aeac03eefb85dc6e13d
BuildTools <unconfigured@null.spigotmc.org>
2021-12-14 17:58:09 +0800

    www

--- async_compress.js
+++ async_compress.js
@@ -1,6 +1,6 @@
 const zstd = require("zstd-codec").ZstdCodec;
 const fs = require("fs");
-const lzw = require("./lzw3.node");
+//const lzw = require("./lzw3.node");
 
 let simple;
 
@@ -16,20 +16,15 @@ function init_compress() {
 }
 
 function compress(data) {
-    let _data = lzw.compress(data);
+    //let _data = lzw.compress(data);
 
-    k.m += _data.length;
+    k.m += data.length;
     k.s += data.length;
 
     return data;
 }
 
 function uncompress(data) {
-    let _data = Buffer.from(simple.decompress(data));
-
-    k.m += data.length;
-    k.s += _data.length;
-
     return data;
 }
 
