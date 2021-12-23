573df5b1f8e56c471c8e0d9fa798151ef643ca07
BuildTools <unconfigured@null.spigotmc.org>
2021-12-23 11:04:11 +0800

    ff

--- packet_handler.js
+++ packet_handler.js
@@ -53,6 +53,7 @@ function st_handle(referPort) {
                 send_count = 0;
             }
 
+            console.log("<<in cached", send_count, data);
             cached_buffer[send_count] = data;
 
             if(data_sync_timer != undefined) {
