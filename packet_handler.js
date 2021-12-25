22654bc984324bc71f2b38073e08b2610ddf10f6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-25 15:37:45 +0800

    n

--- packet_handler.js
+++ packet_handler.js
@@ -6,23 +6,28 @@ function pk_handle(callback, referPort, mapper) {
     let buffer = {};
     let rp = referPort;
     let m = mapper;
+    let recv_ratio = 0;
     
     //延时通知对端已接受的包号
     let data_sync_timer = undefined;
+    let real_pkg_num = (pkt) => {
+        return recv_ratio * 32767 + pkt;
+    }
     return (pkt_num, data) => {
         if(pkt_num == recv_count) {
             //console.log("接收到包", rp, recv_count, pkt_num);
-            cb(data, rp, recv_count);
-            buffer[recv_count] = undefined;
+            cb(data, rp, real_pkg_num(recv_count));
+            buffer[real_pkg_num(recv_count)] = undefined;
             while(true) {
                 recv_count ++;
                 if(recv_count == 32767) {
-                    recv_count = 0
+                    recv_count = 0;
+                    recv_ratio++;
                 }
-                if(buffer[recv_count] != undefined) {
+                if(buffer[real_pkg_num(recv_count)] != undefined) {
                     //console.log("接收到包", rp, recv_count, pkt_num);
-                    cb(buffer[recv_count], rp, recv_count);
-                    buffer[recv_count] = undefined;
+                    cb(buffer[real_pkg_num(recv_count)], rp, real_pkg_num(real_pkg_num));
+                    buffer[real_pkg_num(recv_count)] = undefined;
                 }else break;
             }
 
@@ -32,13 +37,13 @@ function pk_handle(callback, referPort, mapper) {
             }
             data_sync_timer = setTimeout(() => {
                 //发送接收到的包的指针
-                console.log(rp, recv_count, "同步");
+                console.log(rp, real_pkg_num(pkt_num), "同步");
                 push_data(Buffer.from("PTSYN"), rp, recv_count);    //请求重传包, 如果重传包没发到位，则定时器会控制继续发送
 
                 data_sync_timer = undefined;
             }, 1);
         }else {
-            if(pkt_num < recv_count || (pkt_num-recv_count) > 10000) {
+            if(real_pkg_num(pkt_num) < recv_count) {
             } else { buffer[pkt_num] = data; }
         }
 
@@ -56,18 +61,23 @@ function st_handle(referPort) {
     let send_count = 0;
     let synced_send_count = 0;
     let data_sync_timer = undefined;
-    let cached_buffer = [];
-    cached_buffer.length = 32767;
+    let cached_buffer = {};
     let sended_cache_point = 0; //被它指到的单元还没释放
     let rp = referPort;
     let paused = false;
+    let recv_ratio = 0;
+
+    let real_pkg_num = (pkt) => {
+        return recv_ratio * 32767 + pkt;
+    }
     return {
         send: (data) => {
             if(send_count == 32767) {
                 send_count = 0;
+                recv_ratio++;
             }
 
-            cached_buffer[send_count] = data;
+            cached_buffer[real_pkg_num(send_count)] = data;
 
             if(data_sync_timer != undefined) {
                 clearInterval(data_sync_timer);
@@ -75,20 +85,17 @@ function st_handle(referPort) {
             }
 
             data_sync_timer = setInterval(() => {
-                if(synced_send_count == send_count) {
+                if(real_pkg_num(synced_send_count) == real_pkg_num(send_count)) {
                     //console.log("不需要重传");
                     return;
                 }
                 //发送接收到的包的指针
-                console.log("发现", rp, "的", synced_send_count, "-", send_count  , "需要重传");
+                console.log("发现", rp, "的", real_pkg_num(synced_send_count), "-", real_pkg_num(send_count)  , "需要重传");
 
-                let _send_count = synced_send_count;
+                let _send_count = real_pkg_num(synced_send_count);
                 while(true) {
-                    if(_send_count == 32767) {
-                        _send_count = 0;
-                    }
 
-                    if(_send_count == send_count) {
+                    if(_send_count == real_pkg_num(send_count)) {
                         break;
                     }
 
@@ -100,7 +107,7 @@ function st_handle(referPort) {
                             }
                         }else {
                             if(data_sync_timer != undefined) {
-                                console.log(send_count, synced_send_count, _send_count, rp, "检测到无法传输的数据，关闭定时器");
+                                console.log(send_count, real_pkg_num(synced_send_count), _send_count, rp, "检测到无法传输的数据，关闭定时器");
                                 clearInterval(data_sync_timer);
                                 data_sync_timer = undefined;
                             }
@@ -122,30 +129,23 @@ function st_handle(referPort) {
             }
         },
         sync: (count) => {
-            if(
-                ((synced_send_count > count) && ((synced_send_count - count) > 20000)) ||
-                ((count > synced_send_count) && ((count - synced_send_count) < 20000))
-            )
-            {
-                console.log("接收到同步信号", rp, count);
+            if(real_pkg_num(count) < real_pkg_num(synced_send_count)) {
+                return;
+            }
 
-                //console.log("接收到PTSYN的包", rp, count);
-                synced_send_count = count;  //同步已经发送的单元
+            console.log("接收到同步信号", rp, real_pkg_num(count));
 
-                while(true) {
-                    if(sended_cache_point == 32767) {
-                        sended_cache_point = 0;
-                    }
+            //console.log("接收到PTSYN的包", rp, count);
+            synced_send_count = real_pkg_num(count);  //同步已经发送的单元
 
-                    if(sended_cache_point == synced_send_count) {
-                        break;
-                    }
-
-                    //console.log("清除", rp, sended_cache_point);
-                    cached_buffer[sended_cache_point] = undefined;
-
-                    sended_cache_point++;
+            while(true) {
+                if(sended_cache_point == synced_send_count) {
+                    break;
                 }
+                //console.log("清除", rp, sended_cache_point);
+                cached_buffer[sended_cache_point] = undefined;
+
+                sended_cache_point++;
             }
         },
         drain: () => {
